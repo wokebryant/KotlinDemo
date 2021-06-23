7ebd2ad06d6801206e1d41518213498d53540918
luojia <luojia512@163.com>
2021-06-23 14:14:10 +0800

    MotionLayout示例

---
+++ app/src/main/java/com/example/kotlindemo/activity/MotioActivity.kt
@@ -0,0 +1,18 @@
+package com.example.kotlindemo.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.kotlindemo.R
+
+/**
+ *  运动布局
+ */
+class MotionActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_motion)
+    }
+
+
+}
