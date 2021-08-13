c49d100321cd5d25e62e472fc8d180306336ba68
luojia <luojia512@163.com>
2021-08-13 19:17:21 +0800

    update

--- app/src/main/java/com/example/kotlindemo/activity/MotioActivity.kt
+++ app/src/main/java/com/example/kotlindemo/activity/MotioActivity.kt
@@ -1,8 +1,12 @@
 package com.example.kotlindemo.activity
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.constraintlayout.motion.widget.MotionLayout
+import androidx.constraintlayout.widget.ConstraintAttribute
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.example.kotlindemo.R
 import com.example.kotlindemo.utils.dip2px
 import com.example.kotlindemo.widget.FoldTextView
@@ -14,24 +18,51 @@ import kotlinx.android.synthetic.main.fold_title_view.*
  */
 class MotionActivity : TransformActivity() {
 
+    lateinit var titleContainer: MotionLayout
+    lateinit var titleLayout: LinearLayout
+    lateinit var titleTextView: FoldTextView
+    lateinit var titleExpandTv: FoldTextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        setContentView(R.layout.activity_motion)
         setContentView(R.layout.fold_title_view)
 
-        val titleContainer = findViewById<MotionLayout>(R.id.title_container)
-        val titleTextView =findViewById<FoldTextView>(R.id.title_fixed_content_tv)
+        titleContainer = findViewById(R.id.title_container)
+        titleLayout = findViewById(R.id.title_layout)
+        titleTextView =findViewById(R.id.title_fixed_content_tv)
+        titleExpandTv = findViewById(R.id.title_expand_content_tv)
 
         titleTextView.apply {
-            isTipWithTag = true
-            isTipShow = true
+            isShowImage = true
+            isShowTip = true
+            isShowTag = true
+            isShowLightText = true
             mFoldText = "157"
             text = (resources.getText(R.string.title_content))
         }
 
-        titleContainer.getConstraintSet(R.id.start)?.let {
-            it.getConstraint(R.id.title_layout)?.let {
-                it.layout.mHeight = dip2px(44f)
+        titleExpandTv.apply {
+            isShowImage = true
+            isShowTag = true
+            isShowLightText = true
+            isFlash = true
+            text = (resources.getText(R.string.title_content))
+        }
+
+        titleLayout.setOnClickListener {
+            if (titleContainer.progress == 0f) {
+                titleContainer.transitionToEnd()
+            } else if (titleContainer.progress == 1f) {
+                titleContainer.transitionToStart()
+            }
+        }
+
+        titleContainer.setTransitionListener(transListener)
+
+        titleContainer.getConstraintSet(R.id.start)?.let { set ->
+            set.getConstraint(R.id.title_layout)?.let {
+//                it.layout.mHeight = dip2px(44f)
             }
         }
 
@@ -44,6 +75,55 @@ class MotionActivity : TransformActivity() {
             title_progress_bar.setProgress(currentProgress = 20, segmentProgress = 50)
         }
     }
+
+    private val transListener = object : MotionLayout.TransitionListener{
+
+        override fun onTransitionStarted(p0: MotionLayout?, p1: Int, p2: Int) {
+            Log.i("Motion_translation ", " Started")
+        }
+
+        override fun onTransitionChange(
+            p0: MotionLayout?,
+            p1: Int,
+            p2: Int,
+            p3: Float
+        ) {
+        }
+
+        override fun onTransitionCompleted(p0: MotionLayout?, p1: Int) {
+            Log.i("Motion_translation ", " Completed")
+            if (titleContainer.progress == 1.0f) {
+                titleExpandTv.apply {
+//                    text = (resources.getText(R.string.title_content))
+                }
+            }
+        }
+
+        override fun onTransitionTrigger(
+            p0: MotionLayout?,
+            p1: Int,
+            p2: Boolean,
+            p3: Float
+        ) {
+        }
+
+    }
+
+//    private fun changeTitle(isStart: Boolean) {
+//        var content = 0f
+//        if (isStart) {
+//            content = dip2px(11f).toFloat()
+//        } else {
+//            content = dip2px(17f).toFloat()
+//        }
+//        titleContainer.getConstraintSet(R.id.end)?.let { constraintSet ->
+//            constraintSet.getConstraint(R.id.title_fixed_content_tv)?.let {
+//                val attr = ConstraintAttribute("", ConstraintAttribute.AttributeType.FLOAT_TYPE, content)
+//                it.mCustomConstraints["TextSize"] = attr
+//                ConstraintAttribute.setAttributes(titleTextView, it.mCustomConstraints)
+//            }
+//        }
+//    }
 
 
 }
--- app/src/main/java/com/example/kotlindemo/widget/FoldTextView.kt
+++ app/src/main/java/com/example/kotlindemo/widget/FoldTextView.kt
@@ -5,20 +5,24 @@ import android.content.Context
 import android.graphics.*
 import android.text.*
 import android.text.style.ForegroundColorSpan
+import android.text.style.ImageSpan
 import android.util.AttributeSet
 import android.util.Log
 import android.view.MotionEvent
 import android.view.ViewConfiguration
 import android.view.ViewTreeObserver
+import android.view.ViewTreeObserver.OnGlobalLayoutListener
 import androidx.appcompat.widget.AppCompatTextView
 import com.example.kotlindemo.R
 import com.example.kotlindemo.utils.StatusBarUtil
 import com.example.kotlindemo.utils.dip2px
 import kotlinx.android.synthetic.main.activity_motion.view.*
+import java.util.*
 
 /**
  *  可折叠文本
  */
+@Suppress("UNUSED_CHANGED_VALUE")
 class FoldTextView @JvmOverloads constructor(
     context: Context,
     private val attributeSet: AttributeSet? = null,
@@ -31,117 +35,90 @@ class FoldTextView @JvmOverloads constructor(
         private const val MAX_LINE = 2
         private const val EXPAND_TIP_TEXT = "  收起全文"
         private const val FOLD_TIP_TEXT = "全文"
+        private const val IMAGE_SPAN_TEXT = "[]"
         private const val TIP_COLOR = -0x1
         private const val TAG_COLOR = -0x1
         private val BEHAVIOR_COLOR = Color.parseColor("#ffff00")
         private val TARGET_COLOR = Color.parseColor("#01ffce")
+        private val TARGET_HIDE_COLOR = Color.parseColor("#242F35")
+        private const val LIGHT_TEXT_FLASH_NUM = 4
+        private const val LIGHT_TEXT_FLASH_INTERVAL = 300L
         private const val END = 0
     }
 
-    /**
-     *  Tag留白间隔
-     */
-    private val tagInterval = dip2px(6f)
-
-    /**
-     *  Tag圆角
-     */
-    private val tagCorner = dip2px(4f).toFloat()
-
-    /**
-     * 显示最大行数
-     */
-    private var mShowMaxLine: Int = 0
-    /**
-     * 折叠文本
-     */
-    var mFoldText: String? = ""
-
-    /**
-     * 展开文本
-     */
-    private var mExpandText: String? = ""
-    /**
-     * 原始文本
-     */
-    private var mOriginalText: String = ""
-    /**
-     * 是否展开
-     */
-    private var isExpand = false
-    /**
-     * 全文显示的位置 0末尾 1下一行
-     */
-    private var mTipGravity = 0
-    /**
-     * 提示文字颜色
-     */
-    private var mTipColor: Int = 0
-
-    /**
-     * 标签颜色
-     */
-    private var mTagColor: Int = 0
-
-    /**
-     * 提示是否可点击
-     */
-    private var mTipClickable = false
-    private var flag = false
     private var mPaint: Paint = Paint()
 
-    /**
-     *  提示是否展示(不展示的话此时相当于普通的TextView)
-     */
-    var isTipShow = false
+    //Tag留白间隔
+    private val tagInterval = dip2px(6f)
+    //Tag圆角
+    private val tagCorner = dip2px(4f).toFloat()
+    //显示最大行数
+    private var mShowMaxLine: Int = 0
+    //折叠文本
+    var mFoldText: String? = ""
+    //展开文本
+    private var mExpandText: String? = ""
+    //原始文本
+    private var mOriginalText: String = ""
+    //全文显示的位置 0末尾 1下一行
+    private var mTipGravity = 0
+    //提示文字颜色
+    private var mTipColor: Int = 0
+    //标签颜色
+    private var mTagColor: Int = 0
+    //提示是否可点击
+    private var mTipClickable = false
+    private var flag = false
 
-    /**
-     * 展开后是否显示文字提示
-     */
-    private var isShowTipAfterExpand = false
-
-    /**
-     * 提示是否添加背景
-     */
-    var isTipWithTag = false
-
-    /**
-     *  是否展示高亮文本
-     */
-    var isShowLightText = true
-    var behaviorText = "Mark: "
-    var targetText = "ThePeople"
-
-    /**
-     * 提示文字坐标范围
-     */
+    //提示文字坐标范围
     private var minX: Float = 0f
     private var maxX: Float = 0f
     private var minY: Float = 0f
     private var maxY: Float = 0f
 
-    /**
-     * 收起全文不在同一行时，增加一个变量记录坐标
-     */
+    //收起全文不在同一行时，增加一个变量记录坐标
     private var middleY: Float = 0f
-    /**
-     * 原始文本行数
-     */
+    //原始文本行数
     private var originalLineCount = 0
-    /**
-     * 是否超过最大行数
-     */
-    private var isOverMaxLine = false
-
-    /**
-     * 点击时间
-     */
+    //点击时间
     private var clickTime = 0L
 
-    private val span by lazy {SpannableStringBuilder(mOriginalText)}
+    //展开后是否显示文字提示
+    private var isShowTipAfterExpand = false
+    //是否超过最大行数
+    private var isOverMaxLine = false
+    //是否展开
+    private var isExpand = false
+    //是否展示提示
+    var isShowTip = false
+    //是否展示tag,展示tag的前提是isShowTip必须为true
+    var isShowTag = false
+    //是否展示高亮文本
+    var isShowLightText = false
+    //是否闪烁
+    var isFlash = false
+    //是否展示图片
+    var isShowImage = false
+
+    //富文本效果
+    private val textSpan by lazy { SpannableStringBuilder(mOriginalText) }
     private val tagSpan by lazy { RadiusBackgroundSpan(mTagColor, mTipColor, tagCorner) }
     private val behaviorSpan = ForegroundColorSpan(BEHAVIOR_COLOR)
-    private val targetSpan = ForegroundColorSpan(TARGET_COLOR)
+    private var targetSpan = ForegroundColorSpan(TARGET_COLOR)
+    private var imageSpan: CenterImageSpan? = null
+
+    var behaviorText = "Mark: "
+    var targetText = "ThePeople"
+    var currentFlashNum = 0
+
+    private var timer = Timer()
+    private var flashTask: TimerTask? = null
+
+    inner class FlashTask : TimerTask() {
+        override fun run() {
+            startTextFlashAnim()
+        }
+    }
 
     init {
         initAttributes()
@@ -164,12 +141,12 @@ class FoldTextView @JvmOverloads constructor(
             mTipClickable = arr.getBoolean(R.styleable.FoldTextView_tipClickable, false)
             mFoldText = arr.getString(R.styleable.FoldTextView_foldText)
             mExpandText = arr.getString(R.styleable.FoldTextView_expandText)
-            isTipShow = arr.getBoolean(R.styleable.FoldTextView_showTip, false)
+            isShowTip = arr.getBoolean(R.styleable.FoldTextView_showTip, false)
             isShowTipAfterExpand = arr.getBoolean(
                 R.styleable.FoldTextView_showTipAfterExpand,
                 false
             )
-            isTipWithTag = arr.getBoolean(R.styleable.FoldTextView_tipWithTag, false)
+            isShowTag = arr.getBoolean(R.styleable.FoldTextView_tipWithTag, false)
             arr.recycle()
         }
         if (TextUtils.isEmpty(mExpandText)) {
@@ -247,8 +224,7 @@ class FoldTextView @JvmOverloads constructor(
             l = layout
         }
         if (l == null) {
-            viewTreeObserver.addOnGlobalLayoutListener(object :
-                ViewTreeObserver.OnGlobalLayoutListener {
+            viewTreeObserver.addOnGlobalLayoutListener(object : OnGlobalLayoutListener {
                 override fun onGlobalLayout() {
                     viewTreeObserver.removeOnGlobalLayoutListener(this)
                     translateText(layout, type)
@@ -262,19 +238,21 @@ class FoldTextView @JvmOverloads constructor(
     fun translateText(l: Layout, type: BufferType?) {
         //记录原始行数
         originalLineCount = l.lineCount
-        if (isShowLightText) {
-            showForegroundSpan()
-        }
         if (l.lineCount > mShowMaxLine) {
             isOverMaxLine = true
-            val span = SpannableStringBuilder()
+            if (isShowImage) {
+                setImageSpan()
+            } else {
+                textSpan.clear()
+            }
+            val stringBuilder = StringBuilder()
             //获取指定行的开始坐标
             val start = l.getLineStart(mShowMaxLine - 1)
             //获取指定行的结束坐标
             var end = l.getLineVisibleEnd(mShowMaxLine - 1)
             if (mTipGravity == END) {
                 //减去尾部字符串的长度
-                val builder = StringBuilder(ELLIPSIZE_END).append("  ").append(if (isTipShow) mFoldText else "")
+                val builder = StringBuilder(ELLIPSIZE_END).append("  ").append(if (isShowTip) mFoldText else "")
                 end -= paint.breakText(
                     mOriginalText,
                     start,
@@ -286,74 +264,139 @@ class FoldTextView @JvmOverloads constructor(
             } else {
                 end--
             }
-            //获取裁剪后的文本,如果显示标签的话，需要额外减去1，预留一个字符串的空间
-            val ellipsize = if (isTipWithTag) mOriginalText.subSequence(0, end - 1) else mOriginalText.subSequence(
-                0,
-                end
-            )
-            //裁剪后的文本和...完成拼接
-            span.append(ellipsize).append(ELLIPSIZE_END)
-            if (mTipGravity != END) {
-                span.append("\n")
+            //TODO 获取裁剪后的文本,如果显示标签的话，需要额外减去1，预留一个字符串的空间
+            var ellipsize = if (isShowTag) mOriginalText.subSequence(0, end - 1) else mOriginalText.subSequence(0, end)
+            //如果显示图片，需要裁剪图片字符
+            if (isShowImage) {
+                ellipsize = ellipsize.toString().replace(IMAGE_SPAN_TEXT, "")
             }
-            super.setText(span, type)
+            //裁剪后的文本和...完成拼接
+            stringBuilder.append(ellipsize).append(ELLIPSIZE_END)
+            if (mTipGravity != END) {
+                stringBuilder.append("\n")
+            }
+            textSpan.append(stringBuilder)
+            //如果对象文本被裁剪，endIndex要单独计算
+            if (isShowLightText) {
+                showForegroundSpan()
+            }
+            super.setText(textSpan, type)
         } else {
             //针对非裁剪情况下tag展示
-            if (isTipWithTag) {
-                showBackgroundSpan()
+            if (isShowImage) {
+                setImageSpan()
             }
-            super.setText(span, BufferType.NORMAL)
+            if (isShowTag) {
+                showTagSpan()
+            }
+            if (isShowLightText) {
+                showForegroundSpan()
+            }
+            super.setText(textSpan, BufferType.NORMAL)
         }
     }
 
+    private fun handleFullTextInMaxLine() {
+        //TODO 针对，一行，两行顶到头，还要展示标签的情况
+    }
+
     /**
-     *  展示背景Span
+     *  展示tag Span,针对没超过最大行数的情况
      */
-    private fun showBackgroundSpan() {
+    private fun showTagSpan() {
         val spaceLength = 2
         val startIndex = mOriginalText.length + spaceLength
         val endIndex = startIndex + mFoldText!!.length
-        span.append("  $mFoldText")
-        span.setSpan(tagSpan, startIndex, endIndex, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        textSpan.append(
+            if (isShowImage) {
+                mOriginalText.replace(IMAGE_SPAN_TEXT, "")
+            } else {
+                mOriginalText
+            })
+        textSpan.append("  $mFoldText")
+        textSpan.setSpan(tagSpan, startIndex, endIndex, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
     }
 
     /**
      *  展示前景Span
      */
-    private fun showForegroundSpan() {
+    private fun showForegroundSpan(cutEndTargetIndex: Int = 0) {
+        if (!isOverMaxLine && !isShowTag) {
+            textSpan.append(
+                if (isShowImage) {
+                    mOriginalText.replace(IMAGE_SPAN_TEXT, "")
+                } else {
+                    mOriginalText
+                })
+        }
+        setBehaviorSpan()
+        setTargetSpan(cutEndTargetIndex)
+
+        if (isFlash) {
+            flashTask?.cancel()
+            flashTask = FlashTask()
+            timer.schedule(flashTask, LIGHT_TEXT_FLASH_INTERVAL, LIGHT_TEXT_FLASH_INTERVAL)
+        }
+    }
+
+    private fun setBehaviorSpan() {
         val startBehaviorIndex = mOriginalText.indexOf(behaviorText)
         val endBehaviorIndex = startBehaviorIndex + behaviorText.length
-        span.setSpan(
+        textSpan.setSpan(
             behaviorSpan,
             startBehaviorIndex,
             endBehaviorIndex,
             Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
         )
+    }
 
+    private fun setTargetSpan(cutEndTargetIndex: Int = 0) {
         val startTargetIndex = mOriginalText.indexOf(targetText)
-        val endTargetIndex = startTargetIndex + targetText.length
-        span.setSpan(
+        val endTargetIndex = if (cutEndTargetIndex == 0) {
+            startTargetIndex + targetText.length
+        } else {
+            cutEndTargetIndex
+        }
+        textSpan.setSpan(
             targetSpan,
             startTargetIndex,
             endTargetIndex,
             Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
         )
-
-        getEmptyByte()
     }
 
-    private fun getEmptyByte() {
-        val length = targetText.length
-        val stringBuilder = StringBuilder()
-        val test = StatusBarUtil.ToDBC(targetText)
-        Log.i("LUOJIA: ", " lenght $length, ${test.length}")
-//        for (i in 0 until length) {
-//            stringBuilder.append()
-//        }
-//        chars.forEachIndexed { index, c ->
-//
-//            chars[0]= "\\0"
-//        }
+    /**
+     *  高亮文本闪烁效果
+     */
+    private fun startTextFlashAnim() {
+        if (currentFlashNum >= LIGHT_TEXT_FLASH_NUM) {
+            flashTask?.cancel()
+            return
+        }
+        currentFlashNum ++
+
+        val currentColor = targetSpan.foregroundColor
+        val nextColor = if (currentColor == TARGET_COLOR) TARGET_HIDE_COLOR else TARGET_COLOR
+
+        targetSpan = ForegroundColorSpan(nextColor)
+        setTargetSpan()
+
+        super.setText(textSpan, BufferType.NORMAL)
+    }
+
+    /**
+     *  拼接图片
+     */
+    @SuppressLint("UseCompatLoadingForDrawables")
+    private fun setImageSpan() {
+        textSpan.clear()
+        val imageStr = SpannableString(IMAGE_SPAN_TEXT)
+        val drawable = resources.getDrawable(R.drawable.ic_question_white)
+        drawable.setBounds(0, 0, dip2px(15.6f), dip2px(15.6f))
+        imageSpan = CenterImageSpan(drawable)
+        imageStr.setSpan(imageSpan, 0, IMAGE_SPAN_TEXT.length, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        textSpan.append(imageStr)
     }
 
     @SuppressLint("DrawAllocation")
@@ -371,7 +414,7 @@ class FoldTextView @JvmOverloads constructor(
             minY = height - (paint.fontMetrics.descent - paint.fontMetrics.ascent) - paddingBottom
             maxY = (height - paddingBottom).toFloat()
 
-            if (isTipWithTag && isTipShow) {
+            if (isShowTag && isShowTip) {
                 val tagRect = RectF(
                     minX - tagInterval,
                     minY,
@@ -389,7 +432,7 @@ class FoldTextView @JvmOverloads constructor(
                     height - paint.fontMetrics.descent - paddingBottom,
                     mPaint.apply { color = mTipColor })
             } else {
-                mFoldText = if (isTipShow) mFoldText else ""
+                mFoldText = if (isShowTip) mFoldText else ""
                 canvas?.drawText(
                     mFoldText!!,
                     minX,
