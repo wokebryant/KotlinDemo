e26dff218819b3a55e06b67aba26eebd6ce4fe25
luojia <luojia.luo@zhaopin.com.cn>
2024-07-31 16:01:55 +0800

    feat: Compsoe分页库列表状态通用Item封装

--- app/src/main/java/com/example/kotlindemo/compose/paging/FetchStateItem.kt
+++ app/src/main/java/com/example/kotlindemo/compose/paging/FetchStateItem.kt
@@ -2,6 +2,7 @@ package com.example.kotlindemo.compose.paging
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material.Button
@@ -9,10 +10,14 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.paging.LoadState
+import com.example.kotlindemo.compose.noRippleClickable
 import com.example.kotlindemo.compose.paging.datasource.FetchState
 import com.example.kotlindemo.compose.widget.refresh.footer.LoadFooter
+import com.zhaopin.social.compose.ui.CustomColor
 
 @Composable
 fun LoadStateItem(composeStateItem: ComposeStateItem) {
@@ -28,25 +33,56 @@ fun LoadStateItem(composeStateItem: ComposeStateItem) {
                 LoadFooter(loadState = LoadState.Loading)
             }
             FetchState.DoneFetching -> {
-                Button(
-                    modifier = Modifier,
-                    onClick = { composeStateItem.retry.invoke() }
-                ) {
-                    Text(text = "加载完成")
-                }
+                LoadEndItem()
             }
             FetchState.FetchingError -> {
-                Button(
-                    modifier = Modifier,
-                    onClick = { composeStateItem.retry.invoke() }
-                ) {
-                    Text(text = "重试")
+                LoadFailItem {
+                    composeStateItem.retry.invoke()
                 }
             }
             FetchState.ReadyToFetch -> {
 
             }
         }
+    }
+}
+
+@Composable
+fun LoadFailItem(onClick: () -> Unit) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(60.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "加载失败，点击重试",
+            style = TextStyle(
+                fontSize = 13.sp,
+                color = CustomColor.C_B1B7BE
+            ),
+            modifier = Modifier.noRippleClickable {
+                onClick.invoke()
+            }
+        )
+    }
+}
+
+@Composable
+fun LoadEndItem() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(60.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "已经到底咯～",
+            style = TextStyle(
+                fontSize =  13.sp,
+                color = CustomColor.C_B1B7BE
+            )
+        )
     }
 }
 
--- app/src/main/java/com/example/kotlindemo/task/appbar/AppBarActivity.kt
+++ app/src/main/java/com/example/kotlindemo/task/appbar/AppBarActivity.kt
@@ -1,10 +1,17 @@
 package com.example.kotlindemo.task.appbar
 
+import android.graphics.Color
 import android.os.Bundle
+import com.example.kotlindemo.R
 import com.example.kotlindemo.base.BaseActivity
 import com.example.kotlindemo.databinding.ActivityAppbarBinding
 import com.example.kotlindemo.utils.binding
+import com.example.kotlindemo.utils.dp
 import com.example.kotlindemo.utils.setVisible
+import com.view.text.addTag
+import com.view.text.config.Orientation
+import com.view.text.config.TagConfig
+import com.view.text.config.Type
 
 /**
  * @Description
@@ -21,6 +28,18 @@ class AppBarActivity : BaseActivity() {
 //            appbar.setVisible()
             appbar2.setViewPager()
 //            appbar2.setViewPager2(lifecycle)
+
+            //3.5 自定义显示位置
+            val tv5Config = TagConfig(Type.IMAGE).apply {
+                imageResource = R.drawable.icon_5
+                imageAlignText = Orientation.RIGHT
+//                text = "绑定"
+                position = textImageTv5.text.length
+//                startGradientBackgroundColor = Color.parseColor("#FFA07A")
+//                endGradientBackgroundColor = Color.parseColor("#FF0040")
+//                textMarginImage = 4.dp.toInt()
+            }
+            textImageTv5.addTag(tv5Config)
         }
     }
 
